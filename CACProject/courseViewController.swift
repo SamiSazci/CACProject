a6a714eed8372fe4546bede4891054a377ca41ec
Muhammad Sohaib Siddiqui <siddiquim@bentonvillek12.org>
2025-10-23 22:40:50 -0500

    add course view controller file

---
+++ CACProject/courseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  courseViewController.swift
+//  CACProject
+//
+//  Created by Siddiqui, Muhammad Sohaib (Student) on 10/23/25.
+//
+
+import UIKit
+
+class courseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
